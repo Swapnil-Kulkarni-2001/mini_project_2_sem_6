e883b0e895b539ed28cb68bd9d4d627f52de584b
Swapnil Kulkarni <swapnilkulkarni987@gmail.com>
2023-04-11 15:09:17 +0530

    login page added

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1>login</h1>
+    </div>
+  )
+}
+
+export default login
